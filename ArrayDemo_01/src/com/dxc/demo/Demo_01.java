fe2841794f1d44349915036870e3c6681ec16357
smadduri5 <smadduri5@IN-5CG0253HJ8>
2020-08-31 11:38:35 +0530

    updated with single line of code

--- ArrayDemo_01/src/com/dxc/demo/Demo_01.java
+++ ArrayDemo_01/src/com/dxc/demo/Demo_01.java
@@ -2,6 +2,7 @@ package com.dxc.demo;
 
 public class Demo_01 {
 	public static void main(String[] args) {
+		System.out.println("updated by msv");
 		System.out.println("this is simple project");
 	}
 

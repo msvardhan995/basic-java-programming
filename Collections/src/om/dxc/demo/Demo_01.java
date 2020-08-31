d077921742379a28218b2249037b444bb2c9fd9f
smadduri5 <smadduri5@IN-5CG0253HJ8>
2020-08-31 11:23:33 +0530

    this is simple project of collection with one line of code

---
+++ Collections/src/om/dxc/demo/Demo_01.java
@@ -0,0 +1,11 @@
+package om.dxc.demo;
+
+public class Demo_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is collection demo class");
+
+	}
+
+}
